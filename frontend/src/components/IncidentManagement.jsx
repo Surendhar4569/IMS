2f0f15245d44fbf31d91456df6e6355c0779b53c
rakeshkoka <rakeshkoka.dev@gmail.com>
2026-07-28 09:38:55 +0530

    Surendhar commit

--- frontend/src/components/IncidentManagement.jsx
+++ frontend/src/components/IncidentManagement.jsx
@@ -1605,7 +1605,7 @@ function Incidents() {
                   className="px-4 py-2 border border-gray-300 text-gray-700 rounded-lg text-sm font-semibold hover:bg-gray-50 transition-all flex items-center gap-2"
                 >
                   <Camera className="w-4 h-4" />
-                  CC TV
+                  CC TVVV
                 </button>
 
                 <button
