4d17e71ed45f3863dc9522eeb6ba9e3ce72612cd
Surendhar4569 <surendharreddy4569@gmail.com>
2026-07-28 17:15:51 +0530

    alter command added

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -188,6 +188,10 @@ const createTables = async () => {
     );
 `)
 
+        await client.query(`
+            alter table incidents
+            add column if not exists checklist_data JSONB;
+       `);
 
         // Create function to update updated_at timestamp
         await client.query(`
